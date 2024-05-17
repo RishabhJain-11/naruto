81ddfd51708dc73d45103d01b31958f0115ed679
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2024-05-17 22:34:21 +0530

    ...

--- routes/ai/index.js
+++ routes/ai/index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const openai = require('../middlewares/openai');
-const { 
-	initMiddleware,
-	creditCheck,
-	contentFilterCheck,
-	sendResponse,
-	creditPayment,
-	saveToHistory,
-}  = require('./middleware');
+const express = require("express");
+const openai = require("../middlewares/openai");
+const {
+  initMiddleware,
+  creditCheck,
+  contentFilterCheck,
+  sendResponse,
+  creditPayment,
+  saveToHistory,
+} = require("./middleware");
 
-let app = express.Router()
+let app = express.Router();
 
-app.use('/', initMiddleware, creditCheck); 
+app.use("/", initMiddleware, creditCheck);
 
-app.use('/', require('./summarize'));
-app.use('/', require('./code/interpret'));
-app.use('/', require('./writing/intro'));
-app.use('/', require('./jobad'));
-app.use('/', require('./helloworld'));
-app.use('/', require('./example'));
-app.use('/', require('./texterrordetection'));
-app.use('/', require('./simpleexplanation'));
-app.use('/', require('./algoexplain'));
-app.use('/', require('./conceptexplainer'));
+// app.use("/", require("../../ignore/routes/ai/summarize"));
+// app.use("/", require("../../ignore/routes/ai/jobad"));
+// app.use("/", require("../../ignore/routes/ai/helloworld"));
+// app.use("/", require("../../ignore/routes/ai/example"));
+app.use("/", require("./code/interpret"));
+app.use("/", require("./writing/intro"));
+app.use("/", require("./texterrordetection"));
+app.use("/", require("./simpleexplanation"));
+app.use("/", require("./algoexplain"));
+app.use("/", require("./conceptexplainer"));
 
-app.use('/', contentFilterCheck); 
-app.use('/', creditPayment); 
-app.use('/', saveToHistory); 
 
-app.use('/', sendResponse); 
+app.use("/", contentFilterCheck);
+app.use("/", creditPayment);
+app.use("/", saveToHistory);
 
-module.exports = app
+app.use("/", sendResponse);
+
+module.exports = app;
--- src/tools/index.js
+++ src/tools/index.js
@@ -1,9 +1,9 @@
-import jobad from './jobad';
-import summarize from './summarize';
+// import jobad from './jobad';
+// import summarize from './summarize';
 import codeInterpret from './interpret';
-import intro from './intro';
-import helloworld from './helloworld';
-import example from './example';
+// import intro from './intro';
+// import helloworld from './helloworld';
+// import example from './example';
 import texterrordetection from './texterrordetection'
 import simpleexplanation from './simpleexplanation'
 import algoexplain from './algoexplain'
@@ -12,11 +12,11 @@ import conceptexplainer from './conceptexplainer'
 const TOOLS = [
 
 	codeInterpret,
-	intro,
-	jobad,
-	summarize,
-	helloworld,
-	example,
+	// intro,
+	// jobad,
+	// summarize,
+	// helloworld,
+	// example,
 	texterrordetection,
 	simpleexplanation,
 	algoexplain,
